04a7f5d7b760cb85b08d054c6daf0fc1e0fdde31
Yusuf <121219831+Gadaffi508@users.noreply.github.com>
2023-03-06 11:25:43 +0300

    Game Manager
    Manager

---
+++ RepeatBackgroundX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBackgroundX : MonoBehaviour
+{
+    private Vector3 startPos;
+    private float repeatWidht;
+    private void Start()
+    {
+        startPos = transform.position;
+        repeatWidht = GetComponent<BoxCollider>().size.x / 2;
+    }
+    private void Update()
+    {
+        if (transform.position.x < startPos.x - repeatWidht)
+        {
+            transform.position = startPos;
+        }
+    }
+
+}
+
+
